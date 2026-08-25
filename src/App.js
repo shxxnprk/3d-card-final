b80e9f4e319d2a2e9fe08e8a33314b0b6dbb2179
shxxnprk <qkrtldhs97@gmail.com>
2026-08-26 05:24:57 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -138,9 +138,9 @@ export default function App() {
       <Lanyard
         position={[0, 0, 20]}
         gravity={[0, -40, 0]}
-        frontImage="https://cdn.imweb.me/upload/S2026072479051d98f7a2d/3c2b3ff729309.png"
-        backImage="https://cdn.imweb.me/upload/S2026072479051d98f7a2d/3c2b3ff729309.png"
-        lanyardImage="https://cdn.imweb.me/upload/S2026072479051d98f7a2d/3c2b3ff729309.png"
+        frontImage="https://cdn.imweb.me/upload/S2026072479051d98f7a2d/e99a6579ef617.png"
+        backImage="https://cdn.imweb.me/upload/S2026072479051d98f7a2d/e02e35112a929.png.png"
+        lanyardImage="https://cdn.imweb.me/upload/S2026072479051d98f7a2d/2d0e4c011a3bc.png"
         lanyardWidth={1.2}
         imageFit="cover"
       />
